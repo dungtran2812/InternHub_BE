b97b99f7517700ee1818a12286234f899ad749bd
ThoNT16 <truongthonguyen107@gmail.com>
2025-01-11 22:27:49 +0700

    feat: init base spring security [trthodt]

---
+++ internhub/src/main/java/com/kalocs/internhub/security/jwt/JwtUtils.java
@@ -0,0 +1,63 @@
+package com.kalocs.internhub.security.jwt;
+
+import java.security.Key;
+import java.util.Date;
+
+import com.kalocs.internhub.security.services.UserDetailsImpl;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+@Log4j2
+public class JwtUtils {
+
+  @Value("${internhub.app.jwtSecret}")
+  private String jwtSecret;
+
+  @Value("${internhub.app.jwtExpirationMs}")
+  private int jwtExpirationMs;
+
+  public String generateJwtToken(Authentication authentication) {
+
+    UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+
+    return Jwts.builder()
+        .setSubject((userPrincipal.getUsername()))
+        .setIssuedAt(new Date())
+        .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+        .signWith(key(), SignatureAlgorithm.HS256)
+        .compact();
+  }
+  
+  private Key key() {
+    return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+  }
+
+  public String getUserNameFromJwtToken(String token) {
+    return Jwts.parserBuilder().setSigningKey(key()).build()
+               .parseClaimsJws(token).getBody().getSubject();
+  }
+
+  public boolean validateJwtToken(String authToken) {
+    try {
+      Jwts.parserBuilder().setSigningKey(key()).build().parse(authToken);
+      return true;
+    } catch (MalformedJwtException e) {
+      log.error("Invalid JWT token: {}", e.getMessage());
+    } catch (ExpiredJwtException e) {
+      log.error("JWT token is expired: {}", e.getMessage());
+    } catch (UnsupportedJwtException e) {
+      log.error("JWT token is unsupported: {}", e.getMessage());
+    } catch (IllegalArgumentException e) {
+      log.error("JWT claims string is empty: {}", e.getMessage());
+    }
+
+    return false;
+  }
+}
