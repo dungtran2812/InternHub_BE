97851249bdfa87397a460bb4f8823ae09ac70703
ThoNT16 <truongthonguyen107@gmail.com>
2025-02-10 18:04:40 +0700

    feat: implement job crud [thont]

---
+++ internhub/src/main/java/com/kalocs/internhub/business/JobFunctionBusiness.java
@@ -0,0 +1,9 @@
+package com.kalocs.internhub.business;
+
+import com.kalocs.internhub.entity.JobFunction;
+
+import java.util.Optional;
+
+public interface JobFunctionBusiness {
+    Optional<JobFunction> getById(int jobFunctionId);
+}
