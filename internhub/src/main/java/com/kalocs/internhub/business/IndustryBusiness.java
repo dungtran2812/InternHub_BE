f161e7ac18cd2c4b9a1599747af9dc0dc08c2cf9
ThoNT16 <truongthonguyen107@gmail.com>
2025-02-10 23:47:47 +0700

    fix: fix missing import [thont]

--- internhub/src/main/java/com/kalocs/internhub/business/IndustryBusiness.java
+++ internhub/src/main/java/com/kalocs/internhub/business/IndustryBusiness.java
@@ -2,6 +2,7 @@ package com.kalocs.internhub.business;
 
 import com.kalocs.internhub.entity.Industry;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IndustryBusiness {
--- internhub/src/main/java/com/kalocs/internhub/business/JobFunctionBusiness.java
+++ internhub/src/main/java/com/kalocs/internhub/business/JobFunctionBusiness.java
@@ -2,6 +2,7 @@ package com.kalocs.internhub.business;
 
 import com.kalocs.internhub.entity.JobFunction;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface JobFunctionBusiness {
