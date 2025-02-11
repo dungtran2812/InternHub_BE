94a95b038f5f18819df7be2bcb7e85583cce812c
ThoNT16 <truongthonguyen107@gmail.com>
2025-02-11 23:52:08 +0700

    feat: add auth utils and refactor exception handler [thont]

--- internhub/src/main/java/com/kalocs/internhub/config/handler/GlobalExceptionHandler.java
+++ internhub/src/main/java/com/kalocs/internhub/config/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.kalocs.internhub.config.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,8 +14,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorMessage> handleGeneralException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR.value(),new Date(), ex.getMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorMessage(HttpStatus.BAD_REQUEST.value(),new Date(), ex.getMessage()));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
@@ -33,5 +34,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorMessage> handleAppException(AppException ex) {
         return ResponseEntity.status(ex.getCode())
                 .body(new ErrorMessage(ex.getCode(), new Date(), ex.getMessage()));
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<ErrorMessage> handleAuthorizationDeniedException(AuthorizationDeniedException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(new ErrorMessage(HttpStatus.FORBIDDEN.value(), new Date(), ex.getMessage()));
     }
 }
