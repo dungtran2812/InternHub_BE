7a4bfa888eafd4ff70bd747183c2bb719470c229
ThoNT16 <truongthonguyen107@gmail.com>
2025-02-20 15:35:16 +0700

    feat: implement company image [thont]

--- internhub/src/main/java/com/kalocs/internhub/payload/request/CompanyRequest.java
+++ internhub/src/main/java/com/kalocs/internhub/payload/request/CompanyRequest.java
@@ -1,8 +1,11 @@
 package com.kalocs.internhub.payload.request;
 
+import com.kalocs.internhub.model.IndustryDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -10,7 +13,10 @@ import lombok.NoArgsConstructor;
 public class CompanyRequest {
     private String name;
     private String address;
-    private String industry;
     private String description;
-    private String image;
+    private List<IndustryDTO> industries;
+    private String logoCompany;
+    private String backgroundCompany;
+    private String website;
+    private List<String> imageUrls;
 }
