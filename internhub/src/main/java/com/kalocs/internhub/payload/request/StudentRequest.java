5673e1c24b5d69c022855b2332a22218181dac59
ThoNT16 <truongthonguyen107@gmail.com>
2025-03-07 22:43:52 +0700

    feat: fix typo [thont]

--- internhub/src/main/java/com/kalocs/internhub/payload/request/StudentRequest.java
+++ internhub/src/main/java/com/kalocs/internhub/payload/request/StudentRequest.java
@@ -17,6 +17,6 @@ public class StudentRequest {
     private String phone;
     private boolean gender;
     private String major;
-    private String avtrUrl;
+    private String avtUrl;
     private double gpa;
 }
